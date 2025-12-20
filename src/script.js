6fdb534f4515fd1906fb7a278aa1f35c4aa0e7e6
ntmack1 <ntmack5@gmail.com>
2025-12-19 23:37:12 -0600

    add box collision animation

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-const BALL_SIZE = 5;
+const BALL_SIZE = 40;
 let ballPosition = { x: 20, y: 30 };
 
 let xSpeed = 4;
@@ -22,9 +22,26 @@ function update() {
     ballPosition.y += ySpeed;
 }
 
+function checkCollision() {
+    let ball = {
+        left: ballPosition.x,
+        right: ballPosition.x + BALL_SIZE,
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE
+    }
+
+    if (ball.left < 0 || ball.right > width) {
+        xSpeed = -xSpeed;
+    }
+
+    if (ball.top < 0 || ball.bottom > height) {
+        ySpeed = -ySpeed;
+    }
+}
 function gameLoop() {
     draw();
     update();
+    checkCollision();
 
     // Call this function again after a timeout
     setTimeout(gameLoop, 30);
